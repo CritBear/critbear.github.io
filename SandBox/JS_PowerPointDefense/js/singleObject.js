87d69411457e37c84d30787c298c1e1a9c10e55e
Kim KyoungMin <30585314+CritBear@users.noreply.github.com>
2019-09-02 13:11:01 +0900

    Add files via upload

---
+++ SandBox/JS_PowerPointDefense/js/singleObject.js
@@ -0,0 +1,260 @@
+var canvas;
+var ctx;
+
+var objectList = {
+	tower: [],
+	enemy: [],
+	bullet: [],
+	UI: [],
+	effect: [],
+};
+
+var images = {};
+
+var gameManager = {
+	
+	images: {
+		money: new Image(),
+		life: new Image(),
+		nextRound: new Image(),
+	},
+	
+	money: undefined,
+	life: undefined,
+	round: undefined,
+	score: undefined,
+	towerCount: undefined,
+	enemyCount: undefined,
+	bulletCount: undefined,
+	effectCount: undefined,
+	spawnQueue: undefined,
+	frameCount: undefined,
+	
+	initialize: function() {
+		
+		this.images.money.src = "images/UI_money.png";
+		this.images.life.src = "images/UI_life.png";
+		this.images.nextRound.src = "images/nextRound.png";
+		
+		towerInfo.loadImage();
+		effectInfo.loadImage();
+		
+		this.money = 4000;
+		this.life = 20;
+		this.round = 0;
+		this.score = 0;
+		this.towerCount = 0;
+		this.enemyCount = 0;
+		this.bulletCount = 0;
+		this.effectCount = 0;
+		this.spawnQueue = [];
+		this.frameCount = 0;
+		
+		this.isEnd = false;
+	},
+	
+	nextRound: function() {
+		
+		this.round++;
+		
+		var round = roundInfo[ this.round - 1 ];
+		for( var i = 0; i < round.num; i++ ) {
+			var spawnInfo = {
+				name: round.name,
+				health: round.health,
+				speed: round.speed,
+				rate: round.rate,
+				reward: round.reward
+			};
+			this.spawnQueue.push( spawnInfo );
+		}
+	},
+	
+	update: function() {
+		
+		if( this.spawnQueue.length > 0 ) {
+			this.frameCount++;
+			if( this.spawnQueue[0].rate < this.frameCount ) {
+				var spawn = this.spawnQueue.shift();
+				this.frameCount = 0;
+				
+				makeEnemy( "basicEnemy" ).setMaxHealth( spawn.health ).setSpeed( spawn.speed ).setReward( spawn.reward ).build();
+			}
+		}
+		
+		if( this.isEnd ) {
+			ctx.globalAlpha = 0.7;
+			ctx.fillStyle = "black";
+			ctx.fillRect( 0, 0, canvas.width, canvas.height );
+			ctx.globalAlpha = 1;
+			ctx.fillStyle = "white";
+			ctx.font = "50px Arial";
+			ctx.fillText( "GAME OVER!", canvas.width/2 - 130, canvas.height/2 - 50 );
+			ctx.font = "40px Consolas";
+			ctx.fillText( "ROUND " + this.round, canvas.width/2 - 50, canvas.height/2 + 50 );
+			ctx.font = "20px Consolas";
+			ctx.fillText( "Restart by 'F5'", canvas.width/2 - 50, canvas.height/2 + 100 );
+		} else {
+			requestAnimationFrame( update );
+		}
+	},
+	
+	addLife: function( value ) {
+		
+		this.life += value;
+		if( this.life <= 0 ) {
+			this.isEnd = true;
+		}
+	},
+};
+
+var mouse = {
+	
+	pos: new Vector2( 100, 50 ),
+	isLocating: false,
+	locatingTower: {},
+	locatable: true,
+	
+	setLocatingTower: function( name ) {
+		
+		this.isLocating = true;
+		this.locatingTower.name = name;
+		this.locatingTower.range = towerInfo[ name ].range;
+		this.locatingTower.radius = towerInfo[ name ].radius;
+		this.locatingTower.cost = towerInfo[ name ].cost;
+		this.locatingTower.image = towerInfo[ name ].image;
+	},
+	
+	click: function() {
+		
+		if( this.isLocating && this.locatable ) {
+			makeTower( this.locatingTower.name ).setPos( new Vector2( this.pos.x, this.pos.y ) ).build();
+			gameManager.money -= this.locatingTower.cost;
+			this.isLocating = false;
+		}
+	},
+	
+	update: function() {
+		
+		this.locatable = true;
+		
+		if( this.isLocating ) {
+			if( this.pos.x - this.locatingTower.radius < 0 || this.pos.x + this.locatingTower.radius > 1060 || this.pos.y - this.locatingTower.radius < 0 || this.pos.y + this.locatingTower.radius > 700 ) {
+				this.locatable = false;
+			} else {
+				for( var i = 0; i < map.road.length; i++ ) {
+					if( collisionArcRect( {pos: this.pos, radius: this.locatingTower.radius}, map.road[i] ) ) {
+						this.locatable = false;
+						break;
+					}
+				}
+				if( this.locatable ) {
+					for( var i = 0; i < objectList.tower.length; i++ ) {
+						if( collisionArcArc( {pos: this.pos, radius: this.locatingTower.radius}, objectList.tower[i] ) ) {
+							this.locatable = false;
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		this.render();
+	},
+	
+	render: function() {
+		
+		if( this.isLocating ) {
+			if( !this.locatable ) {
+				ctx.save();
+				ctx.globalAlpha = 0.7;
+				ctx.beginPath();
+				ctx.arc( this.pos.x, this.pos.y, this.locatingTower.radius, 0, Math.PI*2 );
+				ctx.fillStyle = "red";
+				ctx.fill();
+				ctx.restore();
+			}
+			
+			ctx.globalAlpha = 0.3;
+			ctx.beginPath();
+			ctx.arc( this.pos.x, this.pos.y, this.locatingTower.range, 0, Math.PI*2 );
+			ctx.fillStyle = "#333";
+			ctx.fill();
+			ctx.globalAlpha = 1;
+			
+			ctx.drawImage(
+				this.locatingTower.image,
+				this.pos.x - this.locatingTower.radius,
+				this.pos.y - this.locatingTower.radius,
+				this.locatingTower.radius * 2,
+				this.locatingTower.radius * 2
+			);
+		} else {
+			ctx.save();
+			ctx.beginPath();
+			ctx.arc( this.pos.x, this.pos.y, 10, 0, Math.PI*2, false );
+			ctx.fillStyle = "yellow";
+			ctx.fill();
+			ctx.restore();
+		}
+	},
+};
+
+var map = {
+	
+	start: {
+		pos: new Vector2( 100, 50 ),
+		width: 40,
+		height: 20,
+	},
+	end: {
+		pos: new Vector2( 1000, 650 ),
+		width: 40,
+		height: 20,
+	},
+	road: [
+		new Road( new Vector2( 75, 0 ), 50, 300 ),
+		new Road( new Vector2( 75, 300 ), 300, 50 ),
+		new Road( new Vector2( 375, 300 ), 50, 200 ),
+		new Road( new Vector2( 175, 500 ), 250, 50 ),
+		new Road( new Vector2( 175, 550 ), 50, 100 ),
+		new Road( new Vector2( 175, 650 ), 500, 500 ),
+		new Road( new Vector2( 675, 150 ), 50, 550 ),
+		new Road( new Vector2( 725, 150 ), 250, 50 ),
+		new Road( new Vector2( 975, 150 ), 50, 550 ),
+	],
+	turningPoint: [
+		new TurningPoint( new Vector2( 75, 350 ), new Vector2( 1, 0 ) ),
+		new TurningPoint( new Vector2( 425, 300 ), new Vector2( 0, 1 ) ),
+		new TurningPoint( new Vector2( 425, 550 ), new Vector2( -1, 0 ) ),
+		new TurningPoint( new Vector2( 175, 500 ), new Vector2( 0, 1 ) ),
+		new TurningPoint( new Vector2( 175, 700 ), new Vector2( 1, 0 ) ),
+		new TurningPoint( new Vector2( 725, 700 ), new Vector2( 0, -1 ) ),
+		new TurningPoint( new Vector2( 675, 150 ), new Vector2( 1, 0 ) ),
+		new TurningPoint( new Vector2( 1025, 150 ), new Vector2( 0, 1 ) ),
+	],
+	
+	update: function() {
+		
+		this.render();
+	},
+	
+	render: function() {
+		
+		ctx.save();
+		
+		ctx.fillStyle = "#aaa";
+		for( var i = 0; i < this.road.length; i++ ) {
+			var road = this.road[i];
+			ctx.fillRect( road.pos.x, road.pos.y, road.width, road.height );
+		}
+		
+		ctx.fillStyle = "green";
+		ctx.fillRect( this.start.pos.x - this.start.width/2, this.start.pos.y - this.start.height/2, this.start.width, this.start.height );
+		
+		ctx.fillStyle = "red";
+		ctx.fillRect( this.end.pos.x - this.end.width/2, this.end.pos.y - this.end.height/2, this.end.width, this.end.height );
+		
+		ctx.restore();
+	},
+};
---
+++ SandBox/JS_PowerPointDefense/js/tower.js
@@ -0,0 +1,512 @@
+
+function Tower( options ) {
+	
+	this.name = options.name;
+	this.id = options.id;
+	this.pos = options.pos;
+	this.damage = towerInfo[options.name].damage;
+	this.range = towerInfo[options.name].range;
+	this.rate = towerInfo[options.name].rate;
+	this.cost = towerInfo[options.name].cost;
+	this.radius = towerInfo[options.name].radius;
+	this.image = towerInfo[options.name].image;
+	this.bullet = towerInfo[options.name].bullet;
+	
+	this.targetId = undefined;
+	this.fireCool = 0;
+	this.angle = 0;
+	this.upgradeList = [];
+}
+
+Tower.prototype.update = function() {
+	
+};
+Tower.prototype.fire = function() {
+	
+};
+Tower.prototype.upgrade = function( num ) {
+	
+	if( this.upgradeList[num] ) {
+		makeTower( this.upgradeList[num] ).setPos( this.pos ).build();
+		getUIByName( "towerControlPanel" ).able = false;
+		getUIByName( "shopPanel" ).able = true;
+		this.remove();
+	}
+};
+Tower.prototype.sell = function() {
+	
+	gameManager.money += Math.floor( this.cost * 0.8 );
+	getUIByName( "towerControlPanel" ).able = false;
+	getUIByName( "shopPanel" ).able = true;
+	this.remove();
+};
+Tower.prototype.remove = function() {
+
+	for(var i = 0; i < objectList.tower.length; i++) {
+		if(objectList.tower[i] === this) {
+			objectList.tower.splice(i, 1);
+		}
+	}
+};
+Tower.prototype.onClick = function() {
+	
+	getUIByName( "shopPanel" ).able = false;
+	getUIByName( "towerControlPanel" ).able = true;
+	getUIByName( "towerControlPanel" ).targetTower = this;
+};
+Tower.prototype.render = function() {
+	
+	ctx.translate( this.pos.x, this.pos.y );
+	ctx.rotate( this.angle );
+	ctx.translate( -this.pos.x, -this.pos.y );
+	ctx.drawImage( this.image, this.pos.x - this.radius, this.pos.y - this.radius, this.radius*2, this.radius*2 );
+	ctx.translate( this.pos.x, this.pos.y );
+	ctx.rotate( -this.angle );
+	ctx.translate( -this.pos.x, -this.pos.y );
+};
+
+var BasicTower = (function() {
+	
+	function BasicTower( options ) {
+		
+		Tower.call( this, options );
+		this.upgradeList = [ "fastTower", "cannonTower" ];
+	}
+	BasicTower.prototype = Object.create( Tower.prototype );
+	BasicTower.prototype.constructor = BasicTower;
+	
+	BasicTower.prototype.update = function() {
+
+		if( getEnemyById( this.targetId ) ) {
+			var target = getEnemyById( this.targetId );
+			if( this.pos.distance( target.pos ) <= this.range ) {
+				this.angle = Math.atan2( target.pos.y - this.pos.y, target.pos.x - this.pos.x );
+				if( this.fireCool <= 0 ) {
+					this.fire();
+				}
+			} else {
+				this.targetId = undefined;
+			}
+		} else {
+			for( var i = 0; i < objectList.enemy.length; i++ ) {
+				if( !objectList.enemy[i] ) {
+					continue;
+				}
+				if( this.pos.distance( objectList.enemy[i].pos ) <= this.range ) {
+					this.targetId = objectList.enemy[i].id;
+					break;
+				}
+			}
+		}
+		
+		if( this.fireCool > 0 ) {
+			this.fireCool--;
+		}
+		this.render();
+	};
+	BasicTower.prototype.fire = function() {
+
+		var firePosOffset = new Vector2( 45, 0 ).rotate( this.angle );
+		
+		makeBullet( this.bullet ).setPos( this.pos.add( firePosOffset ) ).setTargetId( this.targetId ).setDamage( this.damage ).build();
+		makeEffect( "fire_01" ).setSizeShift( -1 ).setTransparentShift( -1 ).setPos( this.pos.add( firePosOffset ) ).setAngle( this.angle ).build();
+		this.fireCool = this.rate;
+	};
+	
+	return BasicTower;
+})();
+
+var FastTower = (function() {
+	
+	function FastTower( options ) {
+		
+		Tower.call( this, options );
+		this.upgradeList = [];
+		this.fireCount = 0;
+		this.firePosOffset = [ new Vector2( 55, -16 ), new Vector2( 55, 16 ) ];
+	}
+	FastTower.prototype = Object.create( Tower.prototype );
+	FastTower.prototype.constructor = FastTower;
+	
+	FastTower.prototype.update = function() {
+		
+		if( getEnemyById( this.targetId ) ) {
+			var target = getEnemyById( this.targetId );
+			if( this.pos.distance( target.pos ) <= this.range ) {
+				this.angle = Math.atan2( target.pos.y - this.pos.y, target.pos.x - this.pos.x );
+				if( this.fireCool <= 0 ) {
+					this.fire();
+				}
+			} else {
+				this.targetId = undefined;
+			}
+		} else {
+			for( var i = 0; i < objectList.enemy.length; i++ ) {
+				if( !objectList.enemy[i] ) {
+					continue;
+				}
+				if( this.pos.distance( objectList.enemy[i].pos ) <= this.range ) {
+					this.targetId = objectList.enemy[i].id;
+					break;
+				}
+			}
+		}
+		
+		if( this.fireCool > 0 ) {
+			this.fireCool--;
+		}
+		this.render();
+	};
+	FastTower.prototype.fire = function() {
+		
+		var firePosOffset = this.firePosOffset[ this.fireCount ].rotate( this.angle );
+		
+		makeBullet( this.bullet ).setPos( this.pos.add( firePosOffset ) ).setTargetId( this.targetId ).setDamage( this.damage ).build();
+		makeEffect( "fire_01" ).setSizeShift( -1 ).setTransparentShift( -1 ).setPos( this.pos.add( firePosOffset ) ).setAngle( this.angle ).build();
+		this.fireCool = this.rate;
+		this.fireCount = (this.fireCount + 1) % 2;
+	};
+	
+	return FastTower;
+})();
+
+var CannonTower = (function() {
+	
+	function CannonTower( options ) {
+		
+		Tower.call( this, options );
+		this.upgradeList = [ 'railGunTower' ];
+	}
+	CannonTower.prototype = Object.create( Tower.prototype );
+	CannonTower.prototype.constructor = CannonTower;
+	
+	CannonTower.prototype.update = function() {
+		
+		if( getEnemyById( this.targetId ) ) {
+			var target = getEnemyById( this.targetId );
+			if( this.pos.distance( target.pos ) <= this.range ) {
+				this.angle = Math.atan2( target.pos.y - this.pos.y, target.pos.x - this.pos.x );
+				if( this.fireCool <= 0 ) {
+					this.fire();
+				}
+			} else {
+				this.targetId = undefined;
+			}
+		} else {
+			for( var i = 0; i < objectList.enemy.length; i++ ) {
+				if( !objectList.enemy[i] ) {
+					continue;
+				}
+				if( this.pos.distance( objectList.enemy[i].pos ) <= this.range ) {
+					this.targetId = objectList.enemy[i].id;
+					break;
+				}
+			}
+		}
+		
+		if( this.fireCool > 0 ) {
+			this.fireCool--;
+		}
+		this.render();
+	};
+	CannonTower.prototype.fire = function() {
+		
+		var firePosOffset = new Vector2(45, 0).rotate(this.angle);
+		
+		makeBullet( this.bullet ).setPos( this.pos.add( firePosOffset ) ).setTargetId( this.targetId ).setDamage( this.damage ).build();
+		makeEffect( "fire_01" ).setSizeShift( -1 ).setTransparentShift( -1 ).setPos( this.pos.add( firePosOffset ) ).setAngle( this.angle ).build();
+		this.fireCool = this.rate;
+	};
+	
+	return CannonTower;
+})();
+
+var RailGunTower = (function() {
+	
+	function RailGunTower( options ) {
+		
+		Tower.call( this, options );
+		this.upgradeList = [];
+	}
+	RailGunTower.prototype = Object.create( Tower.prototype );
+	RailGunTower.prototype.constructor = RailGunTower;
+	
+	RailGunTower.prototype.update = function() {
+		
+		if( getEnemyById( this.targetId ) ) {
+			var target = getEnemyById( this.targetId );
+			if( this.pos.distance( target.pos ) <= this.range ) {
+				this.angle = Math.atan2( target.pos.y - this.pos.y, target.pos.x - this.pos.x );
+				if( this.fireCool <= 0 ) {
+					this.fire();
+				}
+			} else {
+				this.targetId = undefined;
+			}
+		} else {
+			for( var i = 0; i < objectList.enemy.length; i++ ) {
+				if( !objectList.enemy[i] ) {
+					continue;
+				}
+				if( this.pos.distance( objectList.enemy[i].pos ) <= this.range ) {
+					this.targetId = objectList.enemy[i].id;
+					break;
+				}
+			}
+		}
+		
+		if( this.fireCool > 0 ) {
+			this.fireCool--;
+		}
+		this.render();
+	};
+	RailGunTower.prototype.fire = function() {
+		
+		var firePosOffset = new Vector2(45, 0).rotate(this.angle);
+		
+		makeBullet( this.bullet ).setPos( this.pos.add( firePosOffset ) ).setTargetId( this.targetId ).setDamage( this.damage ).build();
+		makeEffect( "fire_01" ).setSizeShift( -1 ).setTransparentShift( -1 ).setPos( this.pos.add( firePosOffset ) ).setAngle( this.angle ).build();
+		this.fireCool = this.rate;
+	};
+	
+	return RailGunTower;
+})();
+
+var IceTower = (function() {
+	
+	function IceTower( options ) {
+		
+		Tower.call( this, options );
+		this.debuff = [ 'slow' ];
+		this.upgradeList = [ 'frozenTower', 'iceBoltTower' ];
+	}
+	IceTower.prototype = Object.create( Tower.prototype );
+	IceTower.prototype.constructor = IceTower;
+	
+	IceTower.prototype.update = function() {
+		
+		this.angle += 0.03;
+		
+		if(this.fireCool <= 0) {
+			for(var i = 0; i < objectList.enemy.length; i++) {
+				if(!objectList.enemy[i]) {
+					continue;
+				}
+				if(this.pos.distance(objectList.enemy[i].pos) <= this.range) {
+					this.fire();
+					break;
+				}
+			}
+		}
+		
+		if( this.fireCool > 0 ) {
+			this.fireCool--;
+		}
+		this.render();
+	};
+	IceTower.prototype.fire = function() {
+		
+		for(var i = 0; i < objectList.enemy.length; i++) {
+			if(!objectList.enemy[i]) {
+				continue;
+			}
+			var enemy = objectList.enemy[i];
+			if(this.pos.distance(enemy.pos) <= this.range) {
+				enemy.damaged(this.damage);
+				this.debuff.forEach(function(debuff) {
+					enemy.setDebuff(debuff, 50);
+				});
+			}
+		}
+		
+		makeEffect('explosion_01').setSize(this.range * 2,this.range * 2).setPos(this.pos).setTransparentShift(-1).build();
+		this.fireCool = this.rate;
+	};
+	
+	return IceTower;
+})();
+
+var FrozenTower = (function() {
+	
+	function FrozenTower( options ) {
+		
+		Tower.call( this, options );
+		this.debuff = [ 'frozen' ];
+		this.upgradeList = [ 'blizzardTower' ];
+	}
+	FrozenTower.prototype = Object.create( Tower.prototype );
+	FrozenTower.prototype.constructor = FrozenTower;
+	
+	FrozenTower.prototype.update = function() {
+		
+		this.angle += 0.03;
+		
+		if(this.fireCool <= 0) {
+			for(var i = 0; i < objectList.enemy.length; i++) {
+				if(!objectList.enemy[i]) {
+					continue;
+				}
+				if(this.pos.distance(objectList.enemy[i].pos) <= this.range) {
+					this.fire();
+					break;
+				}
+			}
+		}
+		
+		if( this.fireCool > 0 ) {
+			this.fireCool--;
+		}
+		this.render();
+	};
+	FrozenTower.prototype.fire = function() {
+		
+		for(var i = 0; i < objectList.enemy.length; i++) {
+			if(!objectList.enemy[i]) {
+				continue;
+			}
+			var enemy = objectList.enemy[i];
+			if(this.pos.distance(enemy.pos) <= this.range) {
+				enemy.damaged(this.damage);
+				this.debuff.forEach(function(debuff) {
+					enemy.setDebuff(debuff, 50);
+				});
+			}
+		}
+		
+		makeEffect('explosion_03').setSize(this.range * 2,this.range * 2).setPos(this.pos).setTransparentShift(-1).build();
+		this.fireCool = this.rate;
+	};
+	
+	return FrozenTower;
+})();
+
+var IceBoltTower = (function() {
+	
+	function IceBoltTower( options ) {
+		
+		Tower.call( this, options );
+		this.debuff = [ 'slow' ];
+		this.upgradeList = [ 'icicleTower' ];
+	}
+	IceBoltTower.prototype = Object.create( Tower.prototype );
+	IceBoltTower.prototype.constructor = IceBoltTower;
+	
+	IceBoltTower.prototype.update = function() {
+		
+		this.angle += 0.03;
+		
+		if( this.fireCool > 0 ) {
+			this.fireCool--;
+		} else {
+			this.fire();
+		}
+		this.render();
+	};
+	IceBoltTower.prototype.fire = function() {
+		
+		for(var i = 0; i < objectList.enemy.length; i++) {
+			if(!objectList.enemy[i]) {
+				continue;
+			}
+			if(this.pos.distance(objectList.enemy[i].pos) <= this.range) {
+				this.targetId = objectList.enemy[i].id;
+				
+				makeBullet(this.bullet).setPos(this.pos).setTargetId(this.targetId).setDamage(this.damage).build();
+			}
+		}
+		
+		this.fireCool = this.rate;
+	};
+	
+	return IceBoltTower;
+})();
+
+
+var SpearTower = (function() {
+	
+	function SpearTower( options ) {
+		
+		Tower.call( this, options );
+		this.upgradeList = [];
+	}
+	SpearTower.prototype = Object.create( Tower.prototype );
+	SpearTower.prototype.constructor = SpearTower;
+	
+	SpearTower.prototype.update = function() {
+		
+		if( getEnemyById( this.targetId ) ) {
+			var target = getEnemyById( this.targetId );
+			if( this.pos.distance( target.pos ) <= this.range ) {
+				this.angle = Math.atan2( target.pos.y - this.pos.y, target.pos.x - this.pos.x );
+				if( this.fireCool <= 0 ) {
+					this.fire();
+				}
+			} else {
+				this.targetId = undefined;
+			}
+		} else {
+			for( var i = 0; i < objectList.enemy.length; i++ ) {
+				if( !objectList.enemy[i] ) {
+					continue;
+				}
+				if( this.pos.distance( objectList.enemy[i].pos ) <= this.range ) {
+					this.targetId = objectList.enemy[i].id;
+					break;
+				}
+			}
+		}
+		
+		if( this.fireCool > 0 ) {
+			this.fireCool--;
+		}
+		this.render();
+	};
+	SpearTower.prototype.fire = function() {
+		
+		makeBullet(this.bullet).setPos(this.pos).setTargetId(this.targetId).setDamage(this.damage).build();
+		this.fireCool = this.rate;
+	};
+	
+	return SpearTower;
+})();
+
+var IcicleTower = (function() {
+	
+	function IcicleTower( options ) {
+		
+		Tower.call( this, options );
+		this.debuff = [ 'slow', 'frozen' ];
+		this.upgradeList = [];
+	}
+	IcicleTower.prototype = Object.create( Tower.prototype );
+	IcicleTower.prototype.constructor = IcicleTower;
+	
+	IcicleTower.prototype.update = function() {
+		
+		this.angle += 0.03;
+		
+		if( this.fireCool > 0 ) {
+			this.fireCool--;
+		} else {
+			this.fire();
+		}
+		this.render();
+	};
+	IcicleTower.prototype.fire = function() {
+		
+		for(var i = 0; i < objectList.enemy.length; i++) {
+			if(!objectList.enemy[i]) {
+				continue;
+			}
+			if(this.pos.distance(objectList.enemy[i].pos) <= this.range) {
+				this.targetId = objectList.enemy[i].id;
+				
+				makeBullet(this.bullet).setPos(this.pos).setTargetId(this.targetId).setDamage(this.damage).build();
+			}
+		}
+		
+		this.fireCool = this.rate;
+	};
+	
+	return IcicleTower;
+})();
